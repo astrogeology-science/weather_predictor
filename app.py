c5eeb907aeabfba9614991227bd04637b39f98fa
Sid1057 <ivanov.dale@gmail.com>
2019-01-16 00:54:26 +0300

    gitignore updates

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template
+from modules.grabber import get_weather
+
+import os
+
+try:
+    APP_ID = os.environ['APP_ID']
+    print(APP_ID)
+except KeyError:
+    print('Please set APP ID key as environment variable.')
+    raise
+
 app = Flask(__name__)
 
 class CustomFlask(Flask):
@@ -18,7 +29,7 @@ def index():
 
 @app.route('/predict_weather/<string:city>')
 def predict_weather(city):
-    return city
+    return str(get_weather(city, APP_ID))
 
 if __name__ == '__main__':
     print('app {} run'.format(__name__))
--- modules/grabber.py
+++ modules/grabber.py
@@ -1,43 +1,13 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+import requests
 
-data = pd.read_csv("https://raw.githubusercontent.com/shwars/PythonJump/master/Data/climat_russia_cities.csv")
-print(data)
+def get_weather(city, app_id):
+    request_string = \
+        'http://api.openweathermap.org/data/2.5/weather?q='+\
+        city+'&appid='+app_id
 
-data.columns=["City","Lat","Long","TempMin","TempColdest","AvgAnnual","TempWarmest","AbsMax","Precipitation"]
-
-print(data.dtypes)
-for x in ["TempMin","TempColdest","AvgAnnual"]:
-    data[x] = data[x].str.replace('−','-')
-    data = data.apply(pd.to_numeric,errors='ignore')
-    print(data.dtypes)
-
-ax = data.plot(x="Lat",y="AvgAnnual",kind="Scatter")
-ax.set_xlabel("Широта")
-ax.set_ylabel("Среднегодовая температура")
-
-plt.show()
-
-ax=data.plot(x="TempMin",y="AbsMax",kind="scatter")
-ax.set_xlabel("Рекорд отрицательной температуры")
-ax.set_ylabel("Рекорд положительной температуры")
-ax.invert_xaxis()
-plt.show()
-
-data['spread'] = data['TempWarmest'] - data['TempColdest']
-data.nlargest(3,'spread')
-plt.show()
-
-
-import geopy.distance
-
-msk_coords = tuple(data.loc[data["City"]=="Москва"][["Lat","Long"]].iloc[0])
-data["DistMsk"] = data.apply(lambda row : geopy.distance.distance(msk_coords,(row["Lat"],row["Long"])).km,axis=1)
-data.head()
-
-msk = data.loc[data['DistMsk']<300]
-print(msk)
-
-ax=msk.plot(x="City",y=["TempColdest","AvgAnnual","TempWarmest"],kind="bar",stacked="true")
-ax.legend(["Мин","Сред","Макс"],loc='lower right')
-plt.show()
+    r = requests.get(request_string)
+    if r.ok:
+        print(r.json())
+        return r.json()
+    else:
+        return r.status_code
